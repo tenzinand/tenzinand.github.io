8e725560b1f355c27a48f961dc946186009925d6
gyurme <saldongyurme4@gmail.com>
2023-02-09 17:14:13 +0100

    oui

---
+++ script.js.js
@@ -0,0 +1,9 @@
+// Mettre un style Special
+function mettreStyleSpecial() {
+  document.getElementById("monTexte").classList.add('specialColor');
+}
+
+// Enlever le style Special
+function enleveStyleSpecial() {
+  document.getElementById("monTexte").classList.remove('specialColor');
+} 
